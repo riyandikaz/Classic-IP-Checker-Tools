0c6b507de5cdf22861e588f98a45c76d0b988953
Riyandika <kzrsriyandika@gmail.com>
2024-09-08 20:35:41 +0700

    ipzero.py

--- ipzero.py
+++ ipzero.py
@@ -6,11 +6,9 @@ import json
 hostname = input('Masukan Website Tujuan: ')
 ip_address = socket.gethostbyname(hostname)
 
-# Construct the request URL
 request_url = 'https://geolocation-db.com/jsonp/' + ip_address
 response = requests.get(request_url)
 
-# Decode and parse the JSON response
 geolocation = response.content.decode()
 geolocation = geolocation.split("(")[1].strip(")")
 geolocation_dict = json.loads(geolocation)
